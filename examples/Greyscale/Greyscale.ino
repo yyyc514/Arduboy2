ab821e88d58f98d7c80f1769667cc4b06fca9ed3
Josh Goebel <me@joshgoebel.com>
2017-04-01 20:44:38 -0400

    simplify it all with a 16 bit timer since we're just testing

--- examples/Greyscale/Greyscale.ino
+++ examples/Greyscale/Greyscale.ino
@@ -32,19 +32,23 @@ void setTimers(uint16_t loops, bool pause)
   uint8_t low = loops & 0xFF;
   // pause interrupt
   if (pause)
-    TIMSK4 &= ~(1 << TOIE4); // overflow
+    TIMSK3 &= ~(1 << TOIE3); // overflow
 
-  TCCR4A = 0;
-  TCCR4B = 0;
+  TCCR3A = 3;
+  TCCR3B = 3 << 3;
 
   // OCR4D = 1;
-  TC4H = high;  // top two bits are going to be set
-  OCR4C = low; // set top of count range
-  TCCR4B |= 0x08 ; // CK/128 prescaler
+  // TC4H = high;  // top two bits are going to be set
+  // OCR4C = low; // set top of count range
+  OCR3AH = high;
+  OCR3AL = low;
+  TCCR3B |= 2; // CLK / 8
+
+  // TCCR4B |= 0x08 ; // CK/128 prescaler
   // TCCR4B |= 0x09 ; // CK/256 prescaler
 
   // TIMSK4 |= (1 << OCIE4D);
-  TIMSK4 |= (1 << TOIE4); // overflow
+  TIMSK3 |= (1 << TOIE3); // overflow
 }
 
 // 138 FPS
@@ -63,7 +67,8 @@ void setTimers(uint16_t loops, bool pause)
 // }
 
 // uint16_t top = 905;
-uint16_t top = 766;
+// uint16_t top = 766;
+uint16_t top = 12256;
 uint16_t top2 = 67;
 boolean granular = true;
 
@@ -122,31 +127,32 @@ uint8_t render_mode = 0;
 // CK/256 counts to 16192us (every 16us)
 
 
-ISR(TIMER4_OVF_vect) {
+// ISR(TIMER4_OVF_vect) {
+ISR(TIMER3_OVF_vect) {
   uint8_t high = top >> 8;
   uint8_t low = top & 0xFF;
 
   uint8_t high2 = top2 >> 8;
   uint8_t low2 = top2 & 0xFF;
 
-  TIMSK4 &= ~(1 << TOIE4); // overflow
-  TCCR4B = 0;
-  if (granular) {
-    TC4H = high2;  // top two bits are going to be set
-    OCR4C = low2; // set top of count range
-    TCCR4B = 0x02 + 64 ; // CK prescaler
-    granular = !granular;
-    TCNT4 = 0;
-    TIMSK4 |= (1 << TOIE4); // overflow
-    return;
-  } else {
-    TC4H = high;  // top two bits are going to be set
-    OCR4C = low; // set top of count range
-    TCCR4B = 0x08 + 64; // CK/128 prescaler
-    granular = !granular;
-    TCNT4 = 0;
-  }
-  TIMSK4 |= (1 << TOIE4); // overflow
+  // TIMSK4 &= ~(1 << TOIE4); // overflow
+  // TCCR4B = 0;
+  // if (granular) {
+  //   TC4H = high2;  // top two bits are going to be set
+  //   OCR4C = low2; // set top of count range
+  //   TCCR4B = 0x02 + 64 ; // CK prescaler
+  //   granular = !granular;
+  //   TCNT4 = 0;
+  //   TIMSK4 |= (1 << TOIE4); // overflow
+  //   return;
+  // } else {
+  //   TC4H = high;  // top two bits are going to be set
+  //   OCR4C = low; // set top of count range
+  //   TCCR4B = 0x08 + 64; // CK/128 prescaler
+  //   granular = !granular;
+  //   TCNT4 = 0;
+  // }
+  // TIMSK4 |= (1 << TOIE4); // overflow
 
   if (!render_done) {
     return; }
@@ -317,10 +323,10 @@ void loop() {
     top2 += 1;
   }
 
-  // if (top_changed) {
-  //  setTimers(top, false);
-  //  top_changed = false;
-  // }
+  if (top_changed) {
+   setTimers(top, false);
+   top_changed = false;
+  }
 
   if(render_mode) {
     // arduboy.clear();
