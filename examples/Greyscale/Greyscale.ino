e6c1f8b7d54149bfb1c4503aa464e516535a192d
Josh Goebel <me@joshgoebel.com>
2017-04-01 20:24:38 -0400

    wip

--- examples/Greyscale/Greyscale.ino
+++ examples/Greyscale/Greyscale.ino
@@ -25,6 +25,7 @@ Arduboy2 arduboy;
 //  |   |   |   |   |   |
 
 
+
 void setTimers(uint16_t loops, bool pause)
 {
   uint8_t high = loops >> 8;
@@ -46,7 +47,26 @@ void setTimers(uint16_t loops, bool pause)
   TIMSK4 |= (1 << TOIE4); // overflow
 }
 
-uint16_t top = 650;
+// 138 FPS
+// 1000 / 138 = every 7.246 ms
+// every 7,936us
+
+// CK count to 64us (every 1/16 of a us)
+// CK/16 counts to 1024us (every 1us)
+// CK/32 counts to 2048us (every 2us)
+// CK/64 counts to 4096us (every 4us)
+// CK/128 counts to 8192us (every 8us)
+// CK/256 counts to 16192us (every 16us)
+
+// ISR(TIMER4_COMPD_vect) {
+
+// }
+
+// uint16_t top = 905;
+uint16_t top = 766;
+uint16_t top2 = 67;
+boolean granular = true;
+
 
 // This function runs once in your game.
 // use it for anything that needs to be set only once in your game.
@@ -70,71 +90,117 @@ void setup() {
   SPI.transfer(0xFF);
   // precharge
   SPI.transfer(0xD9);
-  SPI.transfer(0xF1);
+  // SPI.transfer(0xF1);
+  SPI.transfer(0x16);
   arduboy.LCDDataMode();
 
   // 0xD9, 0xF1,
 
-  // setTimers(top, true);
+  setTimers(top, true);
 }
 
-// 126 FPS
-// 1000 / 126 = every 7.936 ms
+
+
+bool greyscale_rendered=false;
+bool white_rendered=false;
+bool render_done = false;
+uint8_t render_mode = 0;
+
+
+// 138 FPS
+// 1000 / 138 = every 7.246 ms
 // every 7,936us
 
+// CK count to 64us (every 1/16 of a us)
+// CK/2 counts to 128us (every 1/8 a us)
+// CK/4 counts to 256us (every 0.25us)
+// CK/8 counts to 512us (every 0.5us)
 // CK/16 counts to 1024us (every 1us)
 // CK/32 counts to 2048us (every 2us)
 // CK/64 counts to 4096us (every 4us)
 // CK/128 counts to 8192us (every 8us)
 // CK/256 counts to 16192us (every 16us)
 
-// ISR(TIMER4_COMPD_vect) {
-
-// }
-
-
-bool greyscale_rendered=false;
-bool white_rendered=false;
-bool render_done = false;
-bool render_mode = false;
 
 ISR(TIMER4_OVF_vect) {
+  uint8_t high = top >> 8;
+  uint8_t low = top & 0xFF;
+
+  uint8_t high2 = top2 >> 8;
+  uint8_t low2 = top2 & 0xFF;
+
+  TIMSK4 &= ~(1 << TOIE4); // overflow
+  TCCR4B = 0;
+  if (granular) {
+    TC4H = high2;  // top two bits are going to be set
+    OCR4C = low2; // set top of count range
+    TCCR4B = 0x02 + 64 ; // CK prescaler
+    granular = !granular;
+    TCNT4 = 0;
+    TIMSK4 |= (1 << TOIE4); // overflow
+    return;
+  } else {
+    TC4H = high;  // top two bits are going to be set
+    OCR4C = low; // set top of count range
+    TCCR4B = 0x08 + 64; // CK/128 prescaler
+    granular = !granular;
+    TCNT4 = 0;
+  }
+  TIMSK4 |= (1 << TOIE4); // overflow
+
   if (!render_done) {
     return; }
 
   arduboy.display();
-  render_mode=!render_mode;
+  render_mode++;
+  render_mode %= 2;
   render_done = false;
 }
 
 void white() {
   // paint shole screen white
   arduboy.clear();
-  for(uint16_t i=128; i<1024; i++) {
+  for(uint16_t i=256; i<1024; i++) {
     arduboy.sBuffer[i]=0xFF;
   }
 
   arduboy.setTextColor(WHITE);
   arduboy.setTextBackground(BLACK);
   arduboy.setCursor(64, 0);
-  arduboy.println(top);
+  arduboy.print(top);
+  arduboy.print(", ");
+  arduboy.println(top2);
   // arduboy.println(millis());
 }
 
+bool toggle;
+
 void greyscale() {
-  for(uint8_t x=0; x<64; x++) {
-    for(uint16_t y=0; y<1024; y+=128) {
-      arduboy.sBuffer[y+x]=0x00;
+  for(uint16_t y=0; y<1024; y+=128) {
+    for(uint8_t x=0; x<64; x++) {
+      // if (toggle) {
+        arduboy.sBuffer[y+x]=00;
+      //   x++;
+      //   arduboy.sBuffer[y+x]=0b01010101;
+      // } else {
+      //   arduboy.sBuffer[y+x]=0b01010101;
+      //   x++;
+      //   arduboy.sBuffer[y+x]=0b10101010;
+      // }
     }
   }
-
+  toggle = !toggle;
   arduboy.setCursor(0, 0);
-  arduboy.println("Some grey");
+  arduboy.println("Some greys");
+  // arduboy.println("more greys");
+  // arduboy.println("Some greys");
+  // arduboy.println("Some greys");
+  // arduboy.println("Some greys");
 
-  arduboy.setCursor(70, 16);
-  arduboy.setTextColor(BLACK);
-  arduboy.setTextBackground(WHITE);
-  arduboy.println("Hello Josh!");
+  // arduboy.setCursor(70, 17);
+  // arduboy.setTextColor(BLACK);
+  // arduboy.setTextBackground(WHITE);
+  // arduboy.println("Hello Josh!");
 }
 
 
@@ -196,8 +262,8 @@ void simple() {
 // our main game loop, this runs once every cycle/frame.
 // this is where our game logic goes.
 void loop() {
-  simple();
-  return;
+  // simple();
+  // return;
 
 
 
@@ -214,11 +280,26 @@ void loop() {
       top-=1;
       top_changed = true;
     }
+
+    if (arduboy.pressed(LEFT_BUTTON)) {
+      top2 -= 1;
+    } else if (arduboy.pressed(RIGHT_BUTTON)) {
+      top2 += 1;
+    }
+
   }
+
   if (arduboy.buttonsState()) {
     held++;
   } else {
     held=0;
+  }
+
+  if (arduboy.justReleased(A_BUTTON)) {
+    arduboy.LCDCommandMode();
+    SPI.transfer(0xAE);
+    SPI.transfer(0xAF);
+    arduboy.LCDDataMode();
   }
 
   // first we clear our screen to black
@@ -230,10 +311,16 @@ void loop() {
     top_changed = true;
   }
 
-  if (top_changed) {
-   setTimers(top, false);
-   top_changed = false;
+  if (arduboy.justReleased(LEFT_BUTTON)) {
+    top2 -= 1;
+  } else if (arduboy.justReleased(RIGHT_BUTTON)) {
+    top2 += 1;
   }
+
+  // if (top_changed) {
+  //  setTimers(top, false);
+  //  top_changed = false;
+  // }
 
   if(render_mode) {
     // arduboy.clear();
